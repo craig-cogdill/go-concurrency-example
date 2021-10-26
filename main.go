a678703b2cb9ff57a9707e7881a9a40bd08be591
Craig Cogdill <craig.cogdill@flowhub.com>
2021-10-26 11:27:55 -0600

    reformat

--- main.go
+++ main.go
@@ -18,52 +18,52 @@ import (
 const minimumThreads = 4
 
 func calculateMaxThreads() int {
-    availableCpus := runtime.NumCPU() - 1
-    if availableCpus > minimumThreads {
-        return availableCpus
-    }
-    return minimumThreads 
+	availableCpus := runtime.NumCPU() - 1
+	if availableCpus > minimumThreads {
+		return availableCpus
+	}
+	return minimumThreads
 }
 
 func configureLogging() {
-    logConfig := log.Default
-    logLevel := os.Getenv("LOG_LEVEL")
-    if len(logLevel) == 0 {
-        logLevel = string(log.Default.Level)
-    }
-    logConfig.Level = log.Level(logLevel)
+	logConfig := log.Default
+	logLevel := os.Getenv("LOG_LEVEL")
+	if len(logLevel) == 0 {
+		logLevel = string(log.Default.Level)
+	}
+	logConfig.Level = log.Level(logLevel)
 	log.Init(logConfig)
 }
 
 func main() {
-    configureLogging()
-    d := death.NewDeath(syscall.SIGINT, syscall.SIGTERM)
-    goRoutines := []io.Closer{}
+	configureLogging()
+	d := death.NewDeath(syscall.SIGINT, syscall.SIGTERM)
+	goRoutines := []io.Closer{}
 
-    // Report on the number of threads
-    numGoroutines := calculateMaxThreads()
-    log.Info(fmt.Sprintf("Spawning %d goroutines... ", numGoroutines))
+	// Report on the number of threads
+	numGoroutines := calculateMaxThreads()
+	log.Info(fmt.Sprintf("Spawning %d goroutines... ", numGoroutines))
 
-    // Create the broadcaster for notifying the threadpool
-    broadcast := broadcast.NewBroadcaster(1)
+	// Create the broadcaster for notifying the threadpool
+	broadcast := broadcast.NewBroadcaster(1)
 
-    // Create and start workers
-    for i := 0; i < numGoroutines; i++ {
-        worker := threadworker.New(broadcast, i)
-        goRoutines = append(goRoutines, worker)
-        worker.Start()
-    }
+	// Create and start workers
+	for i := 0; i < numGoroutines; i++ {
+		worker := threadworker.New(broadcast, i)
+		goRoutines = append(goRoutines, worker)
+		worker.Start()
+	}
 
-    // Start the HTTP server
-    server := server.New(broadcast, numGoroutines)
-    server.Start()
-    goRoutines = append(goRoutines, server)
+	// Start the HTTP server
+	server := server.New(broadcast, numGoroutines)
+	server.Start()
+	goRoutines = append(goRoutines, server)
 
 	err := d.WaitForDeath(goRoutines...)
 	if err != nil {
 		log.Fatalf("failed to cleanly shut down all go routines: %v", err)
 	}
-    broadcast.Discard()
+	broadcast.Discard()
 
 	log.Info("successfully shutdown all go routines")
 }
