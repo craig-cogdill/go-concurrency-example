381ea8f7c0faa559cabd6f019c1fddbcc82c48d0
Craig Cogdill <craig.cogdill@flowhub.com>
2021-10-26 11:04:11 -0600

    thread workers generate a hash on an API request

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,8 +1,15 @@
-package server 
+package server
 
 import (
-	"github.com/vrecan/life"
+	"context"
+	"net/http"
+	"time"
+	"sync"
+
+	"github.com/gorilla/mux"
 	"github.com/grindlemire/log"
+	"github.com/vrecan/life"
+    "github.com/textileio/go-threads/broadcast"
 )
 
 type Server interface {
@@ -10,25 +17,68 @@ type Server interface {
 	Close()
 }
 
-type server struct {
-	*life.Life
+// This function is blocking and should only be called as a goroutine
+func launchHttpServer(server *http.Server) {
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("ListenAndServe() Error: %v", err)
+	}
 }
 
-func New() *server {
+func shutdownHttpServer(server *http.Server) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	return server.Shutdown(ctx)
+}
+
+type server struct {
+	*life.Life
+	router *mux.Router
+    broadcast *broadcast.Broadcaster
+    numSubscribers int
+}
+
+func New(b *broadcast.Broadcaster, listeners int) *server {
 	s := &server{
-		Life:           life.NewLife(),
+		Life:   life.NewLife(),
+        router: mux.NewRouter(),
+        broadcast: b,
+        numSubscribers: listeners, 
 	}
+
+    s.router.HandleFunc("/hash", s.runHashWithThreadWorkers)
 	s.SetRun(s.run)
 	return s 
 }
 
+func (s *server) runHashWithThreadWorkers(w http.ResponseWriter, request *http.Request) {
+
+    var ping sync.WaitGroup
+    ping.Add(s.numSubscribers)
+    s.broadcast.Send(&ping) // signal the workers to do their thing
+    // Wait for all threads to complete their task
+    ping.Wait()
+
+    // Respond
+    w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+    w.Write([]byte("success"))
+}
+
 func (s *server) run() {
     log.Info("Starting server...")
+    httpServer := http.Server{
+		Addr:    ":8181",
+		Handler: s.router,
+	}
 
+    go launchHttpServer(&httpServer)
+
+    // wait for a shutdown signal
 	for {
 		select {
 		case <-s.Done:
             log.Info("Shutting down server...")
+            shutdownHttpServer(&httpServer)
 			return
 		}
 	}
